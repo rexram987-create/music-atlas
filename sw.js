37225dd550205d32daa499c9acd4ca8cea42e9ec
rexram987-create <rexram987@gmail.com>
2026-09-23 14:44:43 +0300

    Explain Georges and qualified Brassens surname origin

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='music-atlas-shell-v28';
+const CACHE='music-atlas-shell-v29';
 const SHELL=['/','/index.html','/app.js','/artist-data.mjs','/manifest.webmanifest','/icon.svg','/icon-192.png','/icon-512.png'];
 
 self.addEventListener('install',event=>{
